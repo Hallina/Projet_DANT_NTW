707ab2cab87d4528080d709127eeee79ad1a7ebf
EgyAlg <yasser.lachachi@etu.upmc.fr>
2019-04-12 16:48:57 +0200

    ajout test unitaire index

---
+++ src/test/java/index/IndexTest.java
@@ -0,0 +1,16 @@
+package index;
+
+import static org.junit.Assert.*;
+
+public class IndexTest {
+
+    @org.junit.Test
+    public void insert() {
+        System.out.println("hello");
+    }
+
+    @org.junit.Test
+    public void get() {
+        System.out.println("world");
+    }
+}
