707c9aead82dbd33731aaa9190270851deb736af
Hallina <gwenaelle.riccobene@gmail.com>
2019-04-12 15:29:34 +0200

    move pom and git ignore

--- src/Controllers/TableController.java
+++ src/Controllers/TableController.java
@@ -3,11 +3,7 @@ package Controllers;
 import index.Table;
 import services.TableService;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/api/table")
